9a9659192bd14cd83f12f2e34c2d3f85ad6fb6ce
viktoria.hrytsiuk <grytsykvika@gmail.com>
2018-11-12 18:04:14 +0200

    add kaprekar method

---
+++ spec/kaprekar_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../part1/kaprekar'
+require 'pry'
+
+RSpec.describe 'kaprekar?' do
+  context 'square k with n-digits' do
+    context 'add right n digits to the left n digits' do
+      context 'resultant sum is k' do
+        let(:k) { 9 }
+
+        it 'returns true' do
+          expect(kaprekar?(k)).to eq true
+        end
+      end
+
+      context 'resultant sum is not k' do
+        let(:k) { 10 }
+
+        it 'returns false' do
+          expect(kaprekar?(k)).to eq false
+        end
+      end
+    end
+  end
+end
