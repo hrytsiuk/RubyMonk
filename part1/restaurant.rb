71543455d11b133550ffcd9ae5b9d883e8bdbc79
viktoria.hrytsiuk <grytsykvika@gmail.com>
2018-11-13 13:45:36 +0200

    change restaurant class

--- part1/restaurant.rb
+++ part1/restaurant.rb
@@ -1,11 +1,15 @@
 class Restaurant
+
+  attr_reader :menu_rice, :menu_noodles
+
   def initialize(menu)
-    @menu = menu
+    @menu_rice = menu[:rice]
+    @menu_noodles = menu[:noodles]
   end
 
   def cost(*orders)
     cost = orders.map do |order|
-      order[:rice] * @menu[:rice] + order[:noodles] * @menu[:noodles]
+      order[:rice] * menu_rice + order[:noodles] * menu_noodles
     end
     cost.inject(0) { |sum, n| sum + n }
   end
